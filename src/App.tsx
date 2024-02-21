2dc026932e170bcf9ef41a43f17541b749992ae4
Sreelal <N9893107@windstream.com>
2024-02-21 16:35:39 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=' bg-orange-400 w-screen h-screen'>
+test
+      </div>
+    </>
+  )
+}
+
+export default App
